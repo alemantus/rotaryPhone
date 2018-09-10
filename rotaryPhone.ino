3d5940ab60a451bb0b16719a7bcaf3bd8ca298f0
alemantus <Alexander@anak.dk>
2018-09-10 21:41:22 +0200

    ny

--- rotaryPhone.ino
+++ rotaryPhone.ino
@@ -2,20 +2,25 @@ int startTimer = 0;
 unsigned long startTime = 0;
 int input = 50;
 int count = 0;
-
+int count2 = 0;
+int phoneVal = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(input, INPUT);
   Serial.println("HELLO");
+  attachInterrupt(digitalPinToInterrupt(20), interruptRising, RISING);
+
 }
 
 void loop() {
-  
+  phoneVal = analogRead(A0);
   while(digitalRead(input) == HIGH){
-    startTime = millis();
-    count++;
-    delay(60);
+    if(count <= 10){
+      startTime = millis();
+      count++;
+      delay(60);
+    }
   }
 
   if((millis()-startTime) > 500){
@@ -23,10 +28,32 @@ void loop() {
         if (count == 10){
           count = 0;
         }
-        Serial.println(count);
+        //Serial.println(count);
         count = 0;
         Serial.read();
       }
       startTime = 0;
   }
 }
+
+void interruptRising(){
+  startTime = millis();
+  if(millis()-startTime < 60){
+    count2++;
+  }
+  
+    if((millis()-startTime) > 500){
+      if(count2!=0){
+        if (count2 == 10){
+          count2 = 0;
+        }
+        Serial.println(count2);
+        count2 = 0;
+        Serial.read();
+      }
+      startTime = 0;
+  }
+}
+
+
+
